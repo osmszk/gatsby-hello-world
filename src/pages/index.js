76053f0b6f6e3beb392aeb4009cfaecdb8b765c1
osuzuki <osuzuki@plegineer.com>
2020-09-25 16:26:29 +0900

    typography plugin検証のため新しいページ追加

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,12 @@ import Header from "../components/header"
 
 export default function Home() {
   return (
-    <div style={{ color: `purple` }}>
-      <Link to="/contact/">Contact</Link>
-      <Header  headerText="About Gatsby"/>
-      <h1>Hello Gatsby!</h1>
-      <p>What a world.</p>
-      <img src="https://source.unsplash.com/random/400x200" alt="" />
+    <div style={{ margin: `3rem auto`, maxWidth: 600 }}>
+      <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
+      <p>
+        What do I like to do? Lots of course but definitely enjoy building
+        websites.
+      </p>
     </div>
-  );
+  )
 }
