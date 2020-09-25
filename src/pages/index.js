7271be3a977f535c7889180ec663be557acd8159
osuzuki <osuzuki@plegineer.com>
2020-09-25 16:29:15 +0900

    Layoutコンポーネントつくってstyleを共通化する

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,15 @@
 import React from "react"
 import { Link } from "gatsby"
-import Header from "../components/header"
+import Layout from "../components/layout"
 
 export default function Home() {
   return (
-    <div style={{ margin: `3rem auto`, maxWidth: 600 }}>
+    <Layout>
       <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
       <p>
         What do I like to do? Lots of course but definitely enjoy building
         websites.
       </p>
-    </div>
+    </Layout>
   )
 }
