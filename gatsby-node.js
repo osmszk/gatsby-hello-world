c22252581865bb3744dacc6552c9342069ac1b5b
osuzuki <osuzuki@plegineer.com>
2020-09-26 08:43:44 +0900

    [7]gatsby-source-filesystem のcreateFilePath と組み合わせて使えば、マークダウンファイル名からPathを生成できる

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,9 @@
-exports.onCreateNode = ({ node }) => {
-  console.log(`Node created of type "${node.internal.type}"`)
+const { createFilePath } = require(`gatsby-source-filesystem`)
+
+exports.onCreateNode = ({ node, getNode }) => {
+  if (node.internal.type === `MarkdownRemark`) {
+    const fileNode = getNode(node.parent)
+    console.log(`Created of type ${node.internal.type} at ${fileNode.relativePath}`)
+    console.log(createFilePath({ node, getNode, basePath: `pages` }))
+  }
 }
